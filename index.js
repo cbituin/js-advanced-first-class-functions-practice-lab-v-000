a1a0b70cd3a3bd4f928ebe27bb5958c9d6a1bcb3
Conrad Bituin <inbetweenwebdesigns@gmail.com>
2019-09-24 19:43:57 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -18,5 +18,5 @@ const driversByRevenue = (arr) => {
 };
 
 const driversByName = (arr) => {
-  return arr.slice().sort((a, b) => (a.color > b.color) ? 1 : -1);
+  return arr.slice().sort((a, b) => (a.name > b.name) ? 1 : -1);
 };
